9312079a0ec64f0bf4a70876e2402e3ac25ab57e
gessner-o-bezerra <gessner.o.bezerra@gmail.com>
2024-08-30 12:02:19 -0300

    Ajuste do css e identação do código

--- src/js/script.js
+++ src/js/script.js
@@ -8,16 +8,17 @@ document.addEventListener("DOMContentLoaded", () => {
   const inputLocation = document.querySelector("#location");
   const inputEpisode = document.querySelector("#episode");
 
-   let characters = [];
+  let characters = [];
   let episodes = [];
   let locations = new Set();
   let currentPage = 1;
-  const charactersPerPage = 4
-   let totalCharactersCount = 0;
-   let totalEpisodesCount = 0;
-  let totalPages = 1
+  const charactersPerPage = 4;
+  let totalCharactersCount = 0;
+  let totalEpisodesCount = 0;
+  let totalPages = 1;
 
-   async function fetchCharacters(page = 1) {
+  // Requisição inicial
+  async function fetchCharacters(page = 1) {
     try {
       const response = await api.get(`/character?page=${page}`);
       characters.push(...response.data.results); // Acumula personagens em vez de sobrescrever
@@ -25,7 +26,10 @@ document.addEventListener("DOMContentLoaded", () => {
       totalCharactersCount = response.data.info.count;
 
       characters.forEach((character) => {
-        if (character.origin.name !== "unknown" && character.origin.name !== "undefined") {
+        if (
+          character.origin.name !== "unknown" &&
+          character.origin.name !== "undefined"
+        ) {
           locations.add(character.origin.name);
         }
       });
@@ -40,7 +44,7 @@ document.addEventListener("DOMContentLoaded", () => {
         updateTotalCharacters();
       }
     } catch (error) {
-      console.error("There was an error fetching the characters:", error);
+      console.error("Ocorreu um erro ao buscar os personagens:", error);
     }
   }
 
@@ -55,10 +59,11 @@ document.addEventListener("DOMContentLoaded", () => {
         await fetchEpisodes(page + 1);
       }
     } catch (error) {
-      console.error("There was an error fetching the episodes:", error);
+      console.error("Ocorreu um erro ao buscar os episódios:", error);
     }
   }
 
+  // Define quantos cards serão exibidos por vez
   function displayCharacters() {
     cardsContainer.innerHTML = "";
     const startIndex = (currentPage - 1) * charactersPerPage;
@@ -77,6 +82,7 @@ document.addEventListener("DOMContentLoaded", () => {
     const cardDiv = document.createElement("div");
     cardDiv.className = "card mb-3";
 
+    // Tradução do status do personagem
     let statusClass;
     let statusNaw;
     let especie;
@@ -92,29 +98,41 @@ document.addEventListener("DOMContentLoaded", () => {
       statusNaw = "Desconhecido";
     }
 
-    if(character.species === "Human"){
-      especie = "Humano"
-    }else{
-      especie = "Alienígena"
+    // Tradução da espécie do personagem
+    if (character.species === "Human") {
+      especie = "Humano";
+    } else {
+      especie = "Alienígena";
     }
 
-    if(character.origin.name === "Earth (Replacement Dimension)"){
-      planet = "Terra (Dimensão de Substituição)"
-    }else if(character.origin.name === "Earth (C-137)"){
-      planet = "Terra (C-137)"
-    }else if(character.origin.name === "unknown" || character.origin.name === "undefined"){
-      planet = "Desconhecido"
+    // Tradução da última localização conhecida do personagem
+    if (character.origin.name === "Earth (Replacement Dimension)") {
+      planet = "Terra (Dimensão de Substituição)";
+    } else if (character.origin.name === "Earth (C-137)") {
+      planet = "Terra (C-137)";
+    } else if (
+      character.origin.name === "unknown" ||
+      character.origin.name === "undefined"
+    ) {
+      console.log(`planeta = ${character.origin.name}`);
+      planet = "Desconhecido";
+      console.log(`planeta traduzido = ${planet}`);
     }
 
-    // Find the last episode
+    // Encontra o último episódio
     const lastEpisodeUrl = character.episode[character.episode.length - 1];
-    const lastEpisode = episodes.find(ep => ep.url === lastEpisodeUrl);
+
+    // Encontra o nome do último episódio onde p personagem foi visto
+    const lastEpisode = episodes.find((ep) => ep.url === lastEpisodeUrl);
     const lastEpisodeName = lastEpisode ? lastEpisode.name : "Desconhecido";
 
+    // Monta o Card
     cardDiv.innerHTML = `
       <div class="row g-0 rounded-4">
         <div class="col-md-3">
-          <img src="${character.image}" class="card-img-top rounded float-start" alt="${character.name}">
+          <img src="${
+            character.image
+          }" class="card-img-top rounded float-start" alt="${character.name}">
         </div>
         <div class="col-md-8">
           <div class="card-body">
@@ -128,7 +146,9 @@ document.addEventListener("DOMContentLoaded", () => {
             <p class="card-label">Visto pela última vez em</p>
             <p class="card-text">${lastEpisodeName}</p>
             <!-- Button trigger modal -->
-            <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#characterModal" data-character='${JSON.stringify(character)}'>
+            <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#characterModal" data-character='${JSON.stringify(
+              character
+            )}'>
               Ver Detalhes
             </button>
           </div>
@@ -137,61 +157,71 @@ document.addEventListener("DOMContentLoaded", () => {
     `;
 
     return cardDiv;
-}
+  }
 
-document.addEventListener("click", function(event) {
+  document.addEventListener("click", function (event) {
     if (event.target.matches("[data-bs-toggle='modal']")) {
-        const character = JSON.parse(event.target.getAttribute("data-character"));
-        const modalHeader = document.querySelector("#characterModal .modal-header");
-        const modalBody = document.querySelector("#characterModal .modal-body");
+      const character = JSON.parse(event.target.getAttribute("data-character"));
+      const modalHeader = document.querySelector(
+        "#characterModal .modal-header"
+      );
+      const modalBody = document.querySelector("#characterModal .modal-body");
 
-        let statusClass;
-        let statusNaw;
-        let especie;
-        let planet;
-        if (character.status === "Alive") {
-            statusClass = "status-alive";
-            statusNaw = "Vivo";
-        } else if (character.status === "Dead") {
-            statusClass = "status-dead";
-            statusNaw = "Morto";
-        } else {
-            statusClass = "status-unknown";
-            statusNaw = "Desconhecido";
-        }
+      let statusClass;
+      let statusNaw;
+      let especie;
+      let planet;
 
-        if(character.species === "Human"){
-            especie = "Humano"
-        }else{
-            especie = "Alienígena"
-        }
+      // Tradução das informações para exibir no modal
+      if (character.status === "Alive") {
+        statusClass = "status-alive";
+        statusNaw = "Vivo";
+      } else if (character.status === "Dead") {
+        statusClass = "status-dead";
+        statusNaw = "Morto";
+      } else {
+        statusClass = "status-unknown";
+        statusNaw = "Desconhecido";
+      }
 
-        if(character.origin.name === "Earth (Replacement Dimension)"){
-            planet = "Terra (Dimensão de Substituição)"
-        }else if(character.origin.name === "Earth (C-137)"){
-            planet = "Terra (C-137)"
-        }else if(character.origin.name === "unknown" || character.origin.name === "undefined"){
-            planet = "Desconhecido"
-        }
+      if (character.species === "Human") {
+        especie = "Humano";
+      } else {
+        especie = "Alienígena";
+      }
 
-        modalHeader.innerHTML = `
+      if (character.origin.name === "Earth (Replacement Dimension)") {
+        planet = "Terra (Dimensão de Substituição)";
+      } else if (character.origin.name === "Earth (C-137)") {
+        planet = "Terra (C-137)";
+      } else if (
+        character.origin.name === "unknown" ||
+        character.origin.name === "undefined"
+      ) {
+        planet = "Desconhecido";
+      }
+
+      modalHeader.innerHTML = `
             <img src="${character.image}" class="mb-2 img-custon-modal" alt="${character.name}">
             <h5>${character.name}</h5>
-        `
-        modalBody.innerHTML = `
+        `;
+      modalBody.innerHTML = `
             
             <p>Status: <span class="status-indicator ${statusClass}"></span> ${statusNaw}</p>
             <p>Espécie: ${especie}</p>
             <p>Planeta: ${planet}</p>
         `;
     }
-});
+  });
 
+  // Lógica para desabilitar os botões Next e Prev
   function updatePaginationButtons() {
     prevButton.disabled = currentPage === 1;
-    nextButton.disabled = currentPage === Math.ceil(characters.length / charactersPerPage);
+    nextButton.disabled =
+      currentPage === Math.ceil(characters.length / charactersPerPage);
   }
 
+  // Lógica para seguir para a próxima página com mais 20 personagens
   function goToNextPage() {
     if (currentPage < Math.ceil(characters.length / charactersPerPage)) {
       currentPage++;
@@ -199,6 +229,7 @@ document.addEventListener("click", function(event) {
     }
   }
 
+  // Lógica para retornar para a página anterior com mais 20 personagens
   function goToPrevPage() {
     if (currentPage > 1) {
       currentPage--;
@@ -206,6 +237,7 @@ document.addEventListener("click", function(event) {
     }
   }
 
+  // Lógica para filtrar card por nome de personagem
   function filterCharacters() {
     const searchTerm = filter.value.toLowerCase();
     const filteredCharacters = characters.filter((character) =>
@@ -217,6 +249,7 @@ document.addEventListener("click", function(event) {
     updatePaginationButtons();
   }
 
+  // Lógica para inserir no footer o total de personagens localizações e episódios capturados da api
   function updateTotalCharacters() {
     totalCharacters.innerHTML = `
     <p id="totalCharacters" class="footer-item-p mb-2 mb-md-0">Personagens:
@@ -225,7 +258,9 @@ document.addEventListener("click", function(event) {
        
     `;
 
-    const locations = new Set(characters.map(character => character.origin.name));
+    const locations = new Set(
+      characters.map((character) => character.origin.name)
+    );
     inputLocation.innerHTML = `
       <p id="totalCharacters" class="footer-item-p mb-2 mb-md-0">LOCALIZAÇÕES:
     <span class="footer-item mb-2 mb-md-0">${locations.size}</span>
@@ -239,11 +274,11 @@ document.addEventListener("click", function(event) {
     `;
   }
 
+  // Eventos para o filtro e os botões de próximo e anterior
   filter.addEventListener("input", filterCharacters);
   nextButton.addEventListener("click", goToNextPage);
   prevButton.addEventListener("click", goToPrevPage);
 
-  // Initial fetch
+  // Busca inicial
   fetchCharacters();
-  
 });
